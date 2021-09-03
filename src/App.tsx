75a522c32015fd721aedc9fd582eac710578501f
Jon Eubank <joneubank@gmail.com>
2021-09-02 23:42:19 -0400

    Canvas render on page with resize

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import Page from './components/Page';
 
-import Sketch, { Config, Params } from './sketch';
+import Sketch from './sketch';
 
 const emptySketch: Sketch = {
   config: {},
@@ -21,22 +21,6 @@ const App = () => {
         <meta name="description" content="Canvas sketch made by code" />
       </Helmet>
       <Page sketch={emptySketch}></Page>
-      <iframe
-        src="https://giphy.com/embed/82CItLnbSh8hzsXK3H"
-        width="268"
-        height="480"
-        frameBorder="0"
-        className="giphy-embed"
-        allowFullScreen
-      ></iframe>
-      <iframe
-        src="https://giphy.com/embed/MWKh2LORVFR8jtX1Lm"
-        width="298"
-        height="480"
-        frameBorder="0"
-        className="giphy-embed"
-        allowFullScreen
-      ></iframe>
     </>
   );
 };
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 import Sketch from '../sketch';
 
-const FullscreenWrapper = styled('div')`
+const FullscreenWrapper = styled.div`
   height: 100%;
   width: 100%;
   position: fixed;
@@ -16,18 +16,87 @@ const FullscreenWrapper = styled('div')`
   align-items: center;
 `;
 
-styled;
+const CanvasWrapper = styled.div`
+  padding: 50px;
+`;
+
+const ShadowFrameCanvas = styled.canvas`
+  height: 100%;
+  width: 100%;
+  display: block;
+  -webkit-box-shadow: 0px 0px 34px 4px rgba(0, 0, 0, 0.7);
+  -moz-box-shadow: 0px 0px 34px 4 rgba(0, 0, 0, 0.7);
+  box-shadow: 0px 0px 34px 4px rgba(0, 0, 0, 0.7);
+`;
 
 const Page = (props: { sketch: Sketch }) => {
+  let canvas: HTMLCanvasElement;
+
+  // Event Handlers
+  let windowResizeHandler: () => void;
+
+  const resize = () => {
+    const width = 2048;
+    const height = 1024;
+    const canvasAspectRatio = width / height;
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const windowAspectRatio = windowWidth / windowHeight;
+
+    let newHeight = height;
+    let newWidth = width;
+
+    // Always render the canvas within the dimensions of the window
+    if (windowAspectRatio > canvasAspectRatio) {
+      const maxDim = window.innerHeight - 50;
+      if (height > maxDim) {
+        newHeight = maxDim;
+        newWidth = (newHeight / height) * width;
+      }
+    } else {
+      const maxDim = window.innerWidth - 50;
+      if (width > maxDim) {
+        newWidth = maxDim;
+        newHeight = (newWidth / width) * height;
+      }
+    }
+
+    canvas.style.height = newHeight + 'px';
+    canvas.style.width = newWidth + 'px';
+  };
+
+  // Page Load Effect
+  useEffect(() => {
+    // TODO: Log formatter
+    console.log('Sketch Page Loading - Hello!');
+
+    // Grab our canvas
+    canvas = document.getElementById('sketch-canvas') as HTMLCanvasElement;
+
+    // Set the canvas size, attach
+    resize();
+    if (!!windowResizeHandler) {
+      // Due to hot reloading when running as dev it is possible to enter this effect multiple times, if so we want to remove the existing handler
+      window.removeEventListener('resize', windowResizeHandler);
+    }
+
+    // event handlers should be defined as functions (not anonymous) so we can remove the event listeners in the future.
+    windowResizeHandler = function () {
+      resize();
+    };
+    window.addEventListener('resize', windowResizeHandler, true);
+  }, []);
   return (
-    <>
-      <FullscreenWrapper>
-        <div id="canvas-wrapper">
-          <canvas data-download="placeholder" id="sketch-canvas"></canvas>
-        </div>
-      </FullscreenWrapper>
+    <FullscreenWrapper>
+      <CanvasWrapper>
+        <ShadowFrameCanvas
+          data-download="placeholder"
+          id="sketch-canvas"
+        ></ShadowFrameCanvas>
+      </CanvasWrapper>
       <a id="canvas-downloader" download=""></a>
-    </>
+    </FullscreenWrapper>
   );
 };
 
