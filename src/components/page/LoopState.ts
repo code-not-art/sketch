6a65a66b589a329a6e163303a27567a65a414fa0
joneubank <joneubank@gmail.com>
2022-09-18 13:14:15 -0400

    Don't count frames while paused

--- src/components/page/LoopState.ts
+++ src/components/page/LoopState.ts
@@ -30,21 +30,26 @@ export default class LoopState {
 
   togglePause() {
     this.paused = !this.paused;
+    console.log(this.paused ? 'Animation Paused' : 'Animation Unpaused');
   }
 
   nextFrame() {
+    if (this.paused || this.finished) {
+      // If paused or finished: no next frame check needed, just return false.
+      return false;
+    }
+
     const requestTime = Date.now();
     const ticks = requestTime - this.lastTime;
+
+    // ticks > 10 is a bit of a hack. Animation Frame requests run at the 1/60th of a second (16 ms) but there is some small variance.
+    // Usually ticks will be 14 to 18, but execeptions occur at startup/reset so checking for very quick draws prevents weird extra frame draws.
     if (ticks > 10) {
       this.frameData.frame += 1;
       this.frameData.frameTime = ticks;
       this.frameData.totalTime += ticks;
 
       this.lastTime = requestTime;
-
-      if (this.paused || this.finished) {
-        return false;
-      }
 
       return true;
     }
