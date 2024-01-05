b63ae02028c85a1042061b6efdec4166eca755d7
Jon Eubank <joneubank@gmail.com>
2024-01-05 15:09:44 -0500

    Add missing exports to sketch index.ts

--- src/sketch/index.ts
+++ src/sketch/index.ts
@@ -1,13 +1,13 @@
 export { default as Sketch } from './Sketch';
-export { default as SketchProps } from './SketchProps';
-export { default as FrameData } from './FrameData';
-export { default as Config } from './Config';
-export {
-  default as Params,
+export * from './Sketch';
+export type { default as SketchProps } from './SketchProps';
+export type { default as FrameData } from './FrameData';
+export { default as Config, PaletteType } from './Config';
+export type { ConfigInput } from './Config';
+export { default as Params, ParameterType } from './Params';
+export type {
   Parameter,
   MultiSelectOptions,
   RangeOptionsObject,
 } from './Params';
 export { default as Palette } from './Palette';
-
-export { PaletteType, ConfigInput } from './Config';
