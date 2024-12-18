7febd12bcf6caa7434ee3621045d2292cbf8df9c
Jon Eubank <joneubank@gmail.com>
2024-12-17 22:46:23 -0500

    Simplify random seed phrase generation based on simplified rng word generator

--- src/utils/phrase.ts
+++ src/utils/phrase.ts
@@ -1,4 +1,5 @@
-import { Random } from '@code-not-art/core';
+import { Random, Utils } from '@code-not-art/core';
+const { array } = Utils;
 /**
  * Generate a short phrase, has the form `adverb adjective noun` where at least the noun will generate and optionally the adverb and adjective will.
  * Some example phrases:
@@ -15,14 +16,14 @@ import { Random } from '@code-not-art/core';
  * @returns string
  */
 // TODO: Allow inputs to control the structure and odds.
-function seedPhrase(rng: Random, joiner?: string) {
+function seedPhrase(rng: Random, options?: { words?: number; joiner?: string }) {
 	rng.push('phrase generation');
-	const _joiner = joiner || ' ';
-	const hasAdjective = rng.bool(0.75);
-	const hasAdverb = hasAdjective && rng.bool(0.6);
-	const adjective = hasAdjective ? `${rng.word('adjective')}${_joiner}` : '';
-	const adverb = hasAdverb ? `${rng.word('adverb')}${_joiner}` : '';
-	const output = `${adverb}${adjective}${rng.word('noun')}`;
+	const joiner = options?.joiner || ' ';
+	const wordCount = options?.words || 3;
+
+	const output = array(wordCount)
+		.map((_) => rng.word())
+		.join(joiner);
 	rng.pop();
 	return output;
 }
