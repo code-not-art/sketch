15f42dfd36a1eea0b9311e5ba70cdaaf58445b29
Jon Eubank <joneubank@gmail.com>
2025-02-09 11:14:59 -0500

    Use react-helmet despite the react19 dependency warning

--- apps/demos/src/App.tsx
+++ apps/demos/src/App.tsx
@@ -1,11 +1,14 @@
 import { FullPageSketchCanvas, ControlPanelElements, SketchDefinition } from '@code-not-art/sketch';
+import { Helmet } from 'react-helmet';
 
 import sketch from './demos/basic.js';
 
 const App = <TParameters extends ControlPanelElements, DataModel extends object>() => (
 	<>
-		<title>Code Sketch</title>
-		<meta name="description" content="HTML Canvas sketch made by code" />
+		<Helmet>
+			<title>Code Sketch</title>
+			<meta name="description" content="HTML Canvas sketch made by code" />
+		</Helmet>
 		<FullPageSketchCanvas sketch={sketch}></FullPageSketchCanvas>
 	</>
 );
